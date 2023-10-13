d613458c84fb11646a74b64ee586f3e41f735c82
urbanykv <matheusurban19@gmail.com>
2023-10-13 04:37:00 -0300

    Portfólio Matheus Urban

--- src/components/projetos-card/projetosCard.js
+++ src/components/projetos-card/projetosCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import './projetosCard.css'
 
     export const projetosDev = [
-        {id: 2, nome: 'Detone o Ralph', tecnologias: 'HTML, CSS e JavaScript', image: 'assets/img/ralph-background.jpg', github: 'https://github.com/urbanykv/consultar-cep', demo: 'https://consultar-cep-omega.vercel.app/'},
+        {id: 2, nome: 'Detone o Ralph', tecnologias: 'HTML, CSS e JavaScript', image: 'assets/img/ralph-background.jpg', github: 'https://github.com/urbanykv/jogo-detona-ralph', demo: 'https://jogo-detona-ralph-one.vercel.app/'},
 
         {id: 3, nome: "Urban's Ristorante", tecnologias: 'HTML(JSX), CSS, JavaScript e React.js', image: 'assets/img/cardapio-digital-background.jpg',
         imageDestaque: 'assets/img/urban-ristorante.png',
